ba509c3133107e4ce0605dc54bac4e1686dd85f4
LittlesProgramer <magda.kocon1@op.pl>
2021-01-16 23:59:41 +0100

    actual 3

--- Klient.java
+++ Klient.java
@@ -2,6 +2,7 @@ public class Klient{
     public static void main(String[] args) {
         System.out.println("hello");
         System.out.println("jo-jo");
+        System.out.println("Maszka");
     }
 }
 
